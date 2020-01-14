ebc172e3271614a853237fc704d265a9b66f5cf9
Leo (중광) Przybylski <r351574nc3@gmail.com>
2020-01-14 12:36:14 -0700

    Adding base notebook project

---
+++ git-notebook/start-notebook.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+
+set -x
+
+wrapper=""
+if [[ "${RESTARTABLE}" == "yes" ]]; then
+  wrapper="run-one-constantly"
+fi
+
+if [[ ! -z "${JUPYTERHUB_API_TOKEN}" ]]; then
+  # launched by JupyterHub, use single-user entrypoint
+  exec /usr/local/bin/start-singleuser.sh "$@"
+elif [[ ! -z "${JUPYTER_ENABLE_LAB}" ]]; then
+  exec jupyter lab "$@"
+elif [[ ! -z "${JUPYTER_ENABLE}" ]]; then
+  exec jupyter notebook "$@"
+else
+  exec /bin/bash
+fi
